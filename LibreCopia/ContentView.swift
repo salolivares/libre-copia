4b2fd1c3fec397883ba3586132b387db58eeea7c
Sal Olivares <git@salolivares.com>
2024-09-09 20:20:42 -0700

    Initial Commit

---
+++ LibreCopia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LibreCopia
+//
+//  Created by Sal Olivares on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LibreCopia/LibreCopiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibreCopiaApp.swift
+//  LibreCopia
+//
+//  Created by Sal Olivares on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LibreCopiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
